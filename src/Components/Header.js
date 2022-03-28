1c8afd21278c194475f8d4e73d101938dd471da7
unaben <unaben@yahoo.com>
2022-03-28 18:04:15 +0100

    header almost completed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,32 @@
-import React from 'react'
-import "../CSS/header.css"
+import React from "react";
+import "../CSS/header.css";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="main-header-container">
+      <ul className="grid-container">
+        <div className="grid-auto-flow">
+          <div className="logo-container">
+            <li className="">Logo</li>
+          </div>
+          <li className="image box">Image</li>
+        </div>
+        <li className="middle-header-text ">
+          <p>
+            <span>Other / </span>FE/UX Test
+          </p>
+        </li>
+        <div className="five-grid-template-columns">
+          <li>
+            <p className="last-header-text">A</p>
+          </li>
+          <button>Log in</button>
+          <li>icon</li>
+          <li>
+            <p className="">47% icon</p>
+          </li>
+        </div>
+      </ul>
+    </div>
+  );
 }
