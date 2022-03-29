db93701de69215355e949870147cb3e350c84cb0
unaben <unaben@yahoo.com>
2022-03-29 10:49:35 +0100

    Created state, imported useEffect and merged both data objects

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import Main from "./Components/Main";
 import playerData from "./Components/Players.json"
-console.log("Json Data: ", playerData);
+console.log("Json Data: ", playerData.Results);
 
 function App() {
+  const [jsonData, setJsonData] = useState([]);  
+  console.log("JsonData: ", jsonData);
+
+  useEffect(() => {
+    const mergedData =
+      playerData.Results &&
+      playerData.Results.map((result) => ({
+        // console.log(result)
+        ...result,
+        ...playerData.Players.find(
+          (player) => player.playerId === result.playerId
+        ),
+      }));
+    setJsonData(mergedData);
+    console.log("merged: ", mergedData);
+  }, []);
+
+
   return (
     <div className="App-container">
       <header className="header">
